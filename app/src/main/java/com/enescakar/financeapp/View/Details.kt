235ef617b9e2b961a9bea55f5ba5d302a02c16fa
enescakar2148 <progames5252@gmail.com>
2023-05-09 00:20:52 +0300

    new activity: Details

---
+++ app/src/main/java/com/enescakar/financeapp/View/Details.kt
@@ -0,0 +1,12 @@
+package com.enescakar.financeapp.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.enescakar.financeapp.R
+
+class Details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
